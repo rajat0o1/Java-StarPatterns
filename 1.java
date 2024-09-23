d511d0f2aa6722f39095897ded3d8ab9039cc5d8
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-23 19:09:03 +0000

    Configuration Done

---
+++ 1.java
@@ -0,0 +1,17 @@
+class Star{
+    public static void main(String[] args){
+        int i,j;
+
+
+        for(i=1;i<3;i++){
+
+
+
+        for (j=1;j<=5;i++){
+            System.out.print("*");
+        }
+
+        System.out.println("\n");
+        }
+    }
+}
